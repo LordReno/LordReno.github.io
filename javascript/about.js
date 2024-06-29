f7522a790f8b6abcd4aa2f446efd389e5c7805f7
LordReno <reno-yuri.camilleri.20@um.edu.mt>
2024-06-29 16:39:34 +0200

    Small Update
    The website is still incomplete, but a few more features have been added.

---
+++ javascript/about.js
@@ -0,0 +1,4 @@
+function toggleTimeline(elementID) {
+    const timeline = document.getElementById(elementID);
+    timeline.style.display = (timeline.style.display === 'none') ? 'flex' : 'none';
+}
